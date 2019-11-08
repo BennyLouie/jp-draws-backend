069a77cdd6d728a134fcb8cefcd873c076c05db5
BennyLouie <bennylouie1412@gmail.com>
2019-11-08 11:51:11 -0500

    created test seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,18 @@
 class User < ApplicationRecord
     has_secure_password
-    has_many :transactions
+    has_many :transactions, dependent: :destroy
     has_many :drawings, through: :transactions
+
+    def password=(new_password)
+        salt = BCrypt::Engine::generate_salt
+        hashed = BCrypt::Engine::hash_secret(new_password, salt)
+        self.password_digest = salt + hashed
+    end
+
+    def authenticate(password)
+        salt = password_digest[0..28]
+        hashed = BCrypt::Engine::hash_secret(password, salt)
+        return nil unless (salt + hashed) == self.password_digest
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Drawing.destroy_all
+Transaction.destroy_all
+User.reset_pk_sequence
+Drawing.reset_pk_sequence
+Transaction.reset_pk_sequence
+
+ben = User.create(first_name: 'Benny', last_name: 'Louie', username: 'benben')
+ben.password = 'benben'
+
+karina = User.create(first_name: 'Karina', last_name: 'Macancela', username: 'macK')
+karina.password = 'macK'
+
+drawing1 = Drawing.create(name: 'New Drawing 1', img: 'SomethingWhatNot.img', price: 45)
+drawing2 = Drawing.create(name: 'New Drawing 2', img: 'SomeOtherThingWhatNot.img', price: 50)
+
+Transaction.create(user: ben, drawing: drawing1)
+Transaction.create(user: karina, drawing: drawing2)
