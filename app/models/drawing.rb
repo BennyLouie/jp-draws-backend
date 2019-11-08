97b0f4bf09ca188a34c0c05151d29088d819ba2d
BennyLouie <bennylouie1412@gmail.com>
2019-11-08 11:26:13 -0500

    added relationships

--- app/models/drawing.rb
+++ app/models/drawing.rb
@@ -1,2 +1,4 @@
 class Drawing < ApplicationRecord
+    has_many :transactions
+    has_many :users, through: :transactions
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :transactions
+    has_many :drawings, through: :transactions
 end
