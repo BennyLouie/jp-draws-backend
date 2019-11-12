f87e22eca7ee7c7a93a32d35ac868b9607c728bf
BennyLouie <bennylouie1412@gmail.com>
2019-11-12 11:13:49 -0500

    added migration column and changed seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_08_162208) do
+ActiveRecord::Schema.define(version: 2019_11_12_160738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_11_08_162208) do
     t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
   end
 
   create_table "transactions", force: :cascade do |t|
